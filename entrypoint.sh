9122f55f2ba78d94ccd28c9169b8a4042d430f7d
Vadim Peretokin <vperetokin@gmail.com>
2020-03-10 07:03:08 +0100

    Allow for apostrophes in commit message

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,5 @@ set -e
 sh -c "git config --global user.name '${USERNAME}' \
       && git config --global user.email '${USEREMAIL}' \
       && git checkout -b ${BRANCH_NAME} \
-      && git add -A && git commit -m '$*' --allow-empty \
+      && git add -A && git commit -m \"$*\" --allow-empty \
       && git push --force -u origin ${BRANCH_NAME}"
